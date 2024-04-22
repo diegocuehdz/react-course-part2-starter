0b4464fa6386ae761b77bd9a3f5da2d879db6991
Dieguinho Pinho <diegocuehdz@gmail.com>
2024-04-22 02:22:00 -0600

    refactor: time calculations

--- game-hub/src/hooks/useGenres.ts
+++ game-hub/src/hooks/useGenres.ts
@@ -1,6 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
 import APIClient, { FetchResponse } from "../services/api-client";
+import ms from "ms";
+
 const apiClient = new APIClient<Genre>("/genres");
 
 export interface Genre {
@@ -13,7 +15,7 @@ const useGenres = () =>
   useQuery<FetchResponse<Genre>>({
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, // 24 h
+    staleTime: ms("24h"), //24 * 60 * 60 * 1000, // 24 h
     initialData: genres,
   });
 
