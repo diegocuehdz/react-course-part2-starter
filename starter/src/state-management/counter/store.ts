d8e14106ec1ea50a20753e35c64793c9a833fe3a
Dieguinho Pinho <diegocuehdz@gmail.com>
2024-04-24 02:51:30 -0600

    chore: add devtools for zustand

--- starter/src/state-management/counter/store.ts
+++ starter/src/state-management/counter/store.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { mountStoreDevtool } from "simple-zustand-devtools";
 
 interface CounterStore {
   counter: number;
@@ -6,8 +7,14 @@ interface CounterStore {
   reset: () => void;
 }
 
-export const useCounterStore = create<CounterStore>((set) => ({
+const useCounterStore = create<CounterStore>((set) => ({
   counter: 0,
   increment: () => set((store) => ({ counter: store.counter + 1 })),
   reset: () => set(() => ({ counter: 0 })),
 }));
+
+if (process.env.NODE_ENV === "development") {
+  mountStoreDevtool("Counter Store", useCounterStore);
+}
+
+export { useCounterStore };
