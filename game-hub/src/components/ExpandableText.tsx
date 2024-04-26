ff9e51ccb6bbcb6a894b814e46027b73a3006b58
Dieguinho Pinho <diegocuehdz@gmail.com>
2024-04-26 01:47:53 -0600

    feat: expandable text

---
+++ game-hub/src/components/ExpandableText.tsx
@@ -0,0 +1,35 @@
+import { Button, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+
+interface IExpandableTextProps {
+  children: string;
+  limit: number;
+}
+
+export const ExpandableText = ({
+  children,
+  limit = 300,
+}: IExpandableTextProps) => {
+  const [expanded, setExpanded] = useState(false);
+
+  if (!children) return null;
+
+  if (children.length <= limit) return <Text>{children} </Text>;
+
+  const summary = expanded ? children : children.substring(0, limit) + "...";
+
+  return (
+    <Text>
+      {summary}
+      <Button
+        marginLeft={1}
+        size={"xs"}
+        fontWeight="bold"
+        colorScheme="yellow"
+        onClick={() => setExpanded(!expanded)}
+      >
+        {expanded ? "Show Less" : "Read More"}
+      </Button>
+    </Text>
+  );
+};
